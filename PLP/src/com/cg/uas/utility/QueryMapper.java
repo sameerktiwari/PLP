f754f39bcacd5578f471cdf7d946f7f9454dd42e
sameer <sameerktiwari@outlook.com>
2017-11-13 15:26:58 +0530

    Sameer added

--- PLP/src/com/cg/uas/utility/QueryMapper.java
+++ PLP/src/com/cg/uas/utility/QueryMapper.java
@@ -2,18 +2,18 @@ package com.cg.uas.utility;
 
 public interface QueryMapper {
 
-	public static final String query1 = "Select user from Users user where user.loginId=:ploginid and"
+	public static final String users = "Select user from Users user where user.loginId=:ploginid and"
 			+ " user.password=:ppwd and role=:prole";
-	public static final String query2 = "Select programs from ProgramsScheduled programs";
+	public static final String programs = "Select programs from ProgramsScheduled programs";
 
-	public static final String query3 = "SELECT e FROM ProgramsOffered e WHERE e.programName=:pname";
+	public static final String programDetails = "SELECT e FROM ProgramsOffered e WHERE e.programName=:pname";
 
-	public static final String query4 = "SELECT e FROM Application e WHERE e.applicationId=:pappid";
+	public static final String application = "SELECT e FROM Application e WHERE e.applicationId=:pappid";
 
-	public static final String query5 = "SELECT e FROM Application e WHERE e.scheduledProgramId=:pappid";
+	public static final String applications = "SELECT e FROM Application e WHERE e.scheduledProgramId=:pappid";
 
-	public static final String query6 = "SELECT e FROM ProgramsScheduled e WHERE e.scheduledProgrammeId=:ppid";
+	public static final String programsScheduled = "SELECT e FROM ProgramsScheduled e WHERE e.scheduledProgrammeId=:ppid";
 
-	public static final String query7 = "DELETE FROM ProgramsScheduled c WHERE c.scheduledProgrammeId=:p";
+	public static final String deletePrograms = "DELETE FROM ProgramsScheduled c WHERE c.scheduledProgrammeId=:p";
 
 }
